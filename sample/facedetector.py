c2a0fae7bf92b7d2bb4bd89adb28611a8f793001
erton26 <rton26@gmail.com>
2024-03-25 22:10:46 +0900

    fixed getFaceAngle in facedetector.py

--- sample/facedetector.py
+++ sample/facedetector.py
@@ -5,6 +5,7 @@ import scipy
 from pathlib import Path
 from typing import Tuple, List, Dict
 
+
 class FaceDetector:
     def __init__(self, model_path: Path, data_path: Path, num_faces: int = 1):
         self.model_path = model_path
@@ -13,27 +14,27 @@ class FaceDetector:
 
         video = cv2.VideoCapture(self.data_path)
         self.height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.width  = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 
     def getLeftScreen(self, frame: np.ndarray) -> np.ndarray:
-        frameL = frame[0:self.height, 0:(int)(self.width/2)].copy()
+        frameL = frame[0 : self.height, 0 : (int)(self.width / 2)].copy()
 
         return frameL
-    
+
     def getRightScreen(self, frame: np.ndarray) -> np.ndarray:
-        frameR = frame[0:self.height, (int)(self.width/2):self.width].copy()
+        frameR = frame[0 : self.height, (int)(self.width / 2) : self.width].copy()
 
         return frameR
-    
+
     def getVideoLandmarks(self, mode: str = "left") -> List[Dict[str, any]]:
         options = mp.tasks.vision.FaceLandmarkerOptions(
             base_options=mp.tasks.BaseOptions(model_asset_path=self.model_path),
             running_mode=mp.tasks.vision.RunningMode.VIDEO,
-            num_faces=self.num_faces
+            num_faces=self.num_faces,
         )
 
         detector = mp.tasks.vision.FaceLandmarker.create_from_options(options)
-        
+
         video = cv2.VideoCapture(self.data_path)
 
         frame_num = 0
@@ -43,25 +44,26 @@ class FaceDetector:
             frame_exists, frame = video.read()
 
             if frame_exists:
-                if (mode == "left"):
+                if mode == "left":
                     frame = self.getLeftScreen(frame)
-                elif (mode == "right"):
+                elif mode == "right":
                     frame = self.getRightScreen(frame)
-                
-                #frame_timestamp_ms = int(frame_num / fps * 1000)
+
+                # frame_timestamp_ms = int(frame_num / fps * 1000)
                 frame_timestamp_ms = int(video.get(cv2.CAP_PROP_POS_MSEC))
 
                 mp_images = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-                detection_result = detector.detect_for_video(mp_images, frame_timestamp_ms)
-                
+                detection_result = detector.detect_for_video(
+                    mp_images, frame_timestamp_ms
+                )
+
                 if len(detection_result.face_landmarks) > 0:
                     frame_a = {
-                        "frame_num" : frame_num,
-                        "frame_timestamp" : frame_timestamp_ms,
-                        "frame_landmarks" : detection_result.face_landmarks[0]
+                        "frame_num": frame_num,
+                        "frame_timestamp": frame_timestamp_ms,
+                        "frame_landmarks": detection_result.face_landmarks[0],
                     }
                     video_landmarks.append(frame_a)
-                break
                 frame_num += 1
             else:
                 print(f"{mode} finished")
@@ -70,13 +72,13 @@ class FaceDetector:
         video.release()
 
         return video_landmarks
-    
-    def getR(self, lm:List[any]) -> np.ndarray:
+
+    def getR(self, lm: List[any]) -> np.ndarray:
         """
         Calculate rotation matrix.
         Rotate the FaceMesh by R to face the front
         """
-        img_w = int(self.width/2)
+        img_w = int(self.width / 2)
         img_h = self.height
 
         scale_vec = np.array([img_w, img_h, img_w])
@@ -99,28 +101,26 @@ class FaceDetector:
         R = np.array([x, y, z])
 
         return R
-    
-    def getEulerAngle(self, R:List[float]) -> np.ndarray:
-        r =  scipy.spatial.transform.Rotation.from_matrix(R)
-        angle = r.as_euler("xyz",degrees=True)
+
+    def getEulerAngle(self, R: List[float]) -> np.ndarray:
+        r = scipy.spatial.transform.Rotation.from_matrix(R)
+        angle = r.as_euler("xyz", degrees=True)
 
         return angle
-    
-    def getFaceAngle(self, mode: str = "left"):
-        angle_arr = np.array([0,0,0,0])
-        
-        video_landmarks = self.getVideoLandmarks(self, mode=mode)
+
+    def getFaceAngle(self, mode: str = "left") -> List[Dict[str, any]]:
+        video_head_angles = []
+
+        video_landmarks = self.getVideoLandmarks(mode=mode)
 
         for video_landmark in video_landmarks:
-            #print("frame no: {:d}, eulervector: {}".format(video_landmark['frame_num'], calc_R(video_landmark['frame_landmarks'], 1920, 1080)[2]))
-            R = self.getR(video_landmark['frame_landmarks'])
+            R = self.getR(video_landmark["frame_landmarks"])
             angle = self.getEulerAngle(R)
-            frame_num = np.empty(1)
-            frame_num[0] = video_landmark['frame_num']
-            a = np.concatenate((frame_num, angle), axis=None)
-            angle_arr = np.append(angle_arr, a, axis=None)
-            #print(angle_arr)
-        angle_arr = np.reshape(angle_arr, (-1, 4))
-        angle_arr = np.delete(angle_arr,0,0)
-
-        return angle_arr
+            video_head_angle = video_landmark.copy()
+            video_head_angle.pop("frame_landmarks")
+            video_head_angle['pitch'] = angle[0]
+            video_head_angle['yaw'] = angle[1]
+            video_head_angle['roll'] = angle[2]
+            video_head_angles.append(video_head_angle)
+        
+        return video_head_angles
--- sample/sample.py
+++ sample/sample.py
@@ -1,18 +1,32 @@
 import mediapipe as mp
 import numpy as np
 import cv2
+import pandas as pd
 from pathlib import Path
 from facedetector import FaceDetector
 
 def main():
     data_path = './input/data004/movie.mp4'
     model_path = './model/face_landmarker.task'
+    output_path = './output/data004/'
 
     faceDetector = FaceDetector(model_path, data_path)
 
-    video_landmarks = faceDetector.getVideoLandmarks()
+    #left_landmark = faceDetector.getVideoLandmarks()
+    #print(left_landmark)
+    #print(len(left_landmark))
 
-    video_landmarks2 = faceDetector.getVideoLandmarks(mode="right")
+    video_angle_left = faceDetector.getFaceAngle()
+
+    video_angle_right = faceDetector.getFaceAngle(mode="right")
+
+    video_angle_left_df = pd.DataFrame(video_angle_left)
+
+    video_angle_right_df = pd.DataFrame(video_angle_right)
+
+    video_angle_left_df.to_csv('./left.csv')
+
+    video_angle_right_df.to_csv('./right.csv')
 
 if __name__=="__main__":
     main()
